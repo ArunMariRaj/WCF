17805e7d22efb020090f51ec8a91351e2693decf
ArunMariRaj <r.arunmr@gmail.com>
2016-06-30 01:51:56 +0530

    Added Solution

---
+++ Contracts/Contract.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Contracts
+{
+    [DataContract]
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeRole { get; set; }
+        public string EmployeePay { get; set; }
+    }
+    [ServiceContract]
+    interface IEmployeeOperations
+    {
+        [OperationContract]
+        bool AddEmployee(Employee employee);
+        [OperationContract]
+        Employee ViewEmployee(int Id);
+        [OperationContract]
+        bool EditEmployee(Employee employee);
+        [OperationContract]
+        bool RemoveEmployee(int Id);
+    }
+}
---
+++ ServiceImplementation/ServiceImplementation.cs
@@ -0,0 +1,56 @@
+﻿using Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServiceImplementation
+{
+    public class ServiceImplementation
+    {
+        private static int _id = 10000;
+        private static List<Employee> EmployeeList;
+        public bool AddingAnEmployee(Employee employee)
+        {
+            _id++;
+            Employee employeeToBeAdded = new Employee
+            {
+                EmployeeId = _id,
+                EmployeeName = employee.EmployeeName,
+                EmployeeRole = employee.EmployeeRole,
+                EmployeePay = employee.EmployeePay
+            };
+            EmployeeList.Add(employeeToBeAdded);
+            if (EmployeeList.Contains(employeeToBeAdded))
+            {
+                return true;
+            }
+            else return false;
+        }
+        public Employee viewingAnEmployee(int Id)
+        {
+            if(EmployeeList.Exists(x => x.EmployeeId == Id))
+            {
+                return (Employee)EmployeeList.Select(x => x.EmployeeId == Id);
+            }
+            return null;
+        }
+        public bool EditingAnEmployee(Employee employee)
+        {
+            if (EmployeeList.Exists(x=>x.EmployeeId==employee.EmployeeId))
+            {
+                EmployeeList.RemoveAll(x => x.EmployeeId == employee.EmployeeId);
+                EmployeeList.Add(employee);
+                return true;
+            }
+            return false;
+        }
+        public bool RemovingAnEmployee(int Id)
+        {
+            if (EmployeeList.Exists(x => x.EmployeeId == Id))
+            {
+                EmployeeList.RemoveAll(x => x.EmployeeId == Id);
+                return true;
+            }
+            return false;
+        }
+    }
+}
