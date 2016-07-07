de1947e3cf16f308e6a4562be1fda41d32e777f7
ArunMariRaj <r.arunmr@gmail.com>
2016-07-07 14:55:32 +0530

    Implemented choice based bindings

--- WCF Service/ConsoleClient/Program.cs
+++ WCF Service/ConsoleClient/Program.cs
@@ -1,18 +1,36 @@
-﻿using Contracts;
+using Contracts;
 using System;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 
 namespace ConsoleClient
 {
     class Program
     {
+        public enum BindingTypes
+        {
+            BasicHttpBinding,
+            WSHttpBinding
+        }
         static void Main(string[] args)
         {
-            WSHttpBinding binding = new WSHttpBinding();
-            EndpointAddress endPoint = new EndpointAddress("http://localhost:8085/ConsoleHostTest/serviceAddress");
+            Binding binding = null;
+            EndpointAddress endPoint = null;
+            Console.WriteLine("Choose Binding:\n1.BasicHttpBinding\n2.WSHttpBinding");
+            int choice = int.Parse(Console.ReadLine()) - 1;
+            if (choice == 0)
+            {
+                binding = new BasicHttpBinding();
+                endPoint = new EndpointAddress("http://localhost:8085/ConsoleHostTest/basichttpbinding");
+            }
+            else if (choice == 1)
+            {
+                binding = new WSHttpBinding();
+                endPoint = new EndpointAddress("http://localhost:8085/ConsoleHostTest/wshttpbinding");
+            }
             IEmployeeOperations proxy = ChannelFactory<IEmployeeOperations>.CreateChannel(binding, endPoint);
 
-            Employee employee = new Employee { EmployeeName = "Arun", EmployeePay = "40k", EmployeeRole = "SoftwareEngineer" };
+            Employee employee = new Employee { EmployeeName = "Arun", EmployeePay = "30k", EmployeeRole = "SoftwareEngineer" };
             bool isAdded = proxy.AddEmployee(employee);
             if (isAdded)
             {
