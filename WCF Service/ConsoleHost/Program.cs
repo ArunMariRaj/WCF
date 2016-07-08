726b1aab3c99624c907ffda15990fce34ba26ef9
ArunMariRaj <r.arunmr@gmail.com>
2016-07-08 17:37:07 +0530

    Added a WebHost

--- WCF Service/ConsoleHost/Program.cs
+++ WCF Service/ConsoleHost/Program.cs
@@ -1,6 +1,7 @@
-﻿using Host;
+using Host;
 using System;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 
 namespace ConsoleHost
 {
@@ -8,13 +9,16 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
+            WebServiceHost consoleHostWeb = new WebServiceHost(typeof(RestfulEmployeeOperations));
             ServiceHost consoleHost = new ServiceHost(typeof(EmployeeOperations));
+            consoleHostWeb.Open();
             consoleHost.Open();
             Console.WriteLine("Server Started ...");
             Console.WriteLine("Press a Key to Terminate ...");
             Console.ReadLine();
             Console.WriteLine("Server ShutDown ...");
             consoleHost.Close();
+            consoleHostWeb.Close();
         }
     }
 }
