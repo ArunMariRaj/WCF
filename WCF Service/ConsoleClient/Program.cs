ab55ec99261a9b64c693a5500f69b436fe31ba41
ArunMariRaj <r.arunmr@gmail.com>
2016-07-10 23:06:01 +0530

    Implemented Abstract Factory pattern for determining client bindings at Runtime

--- WCF Service/ConsoleClient/Program.cs
+++ WCF Service/ConsoleClient/Program.cs
@@ -7,40 +7,13 @@ namespace ConsoleClient
 {
     class Program
     {
-        public enum BindingTypes
-        {
-            BasicHttpBinding,
-            WSHttpBinding,
-            NetHttpBinding,
-            NetTcpBinding
-        }
         static void Main()
         {
-            Binding binding = null;
-            EndpointAddress endPoint = null;
             Console.WriteLine("Choose Binding:\n1.BasicHttpBinding\n2.WSHttpBinding\n3.NetHttpBinding\n4.NetTcpBinding");
             int choice = int.Parse(Console.ReadLine()) - 1;
-            if (choice == 0)
-            {
-                binding = new BasicHttpBinding();
-                endPoint = new EndpointAddress("http://localhost:8085/ConsoleHostTest/basichttpbinding");
-            }
-            else if (choice == 1)
-            {
-                binding = new WSHttpBinding();
-                endPoint = new EndpointAddress("http://localhost:8085/ConsoleHostTest/wshttpbinding");
-            }
-            else if (choice == 2)
-            {
-                binding = new NetHttpBinding();
-                endPoint = new EndpointAddress("http://localhost:8085/ConsoleHostTest/nethttpbinding");
-            }
-            else if (choice == 3)
-            {
-                binding = new NetTcpBinding();
-                endPoint = new EndpointAddress("net.Tcp://localhost:8086/ConsoleHostTest/nettcpbinding");
-            }
-            IEmployeeOperations proxy = ChannelFactory<IEmployeeOperations>.CreateChannel(binding, endPoint);
+
+            ClientBindings clientBindings = BindingFactory.GetBindingFactory((BindingTypes)choice);
+            IEmployeeOperations proxy = ChannelFactory<IEmployeeOperations>.CreateChannel(clientBindings.BindingType, clientBindings.EndPoint);
 
             Employee employee = new Employee { EmployeeName = "Arun", EmployeePay = "30k", EmployeeRole = "SoftwareEngineer" };
             bool isAdded = proxy.AddEmployee(employee);
