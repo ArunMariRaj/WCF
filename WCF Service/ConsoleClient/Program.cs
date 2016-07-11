40e04251bdb2805e9c799e51e17b5cc51a54d454
ArunMariRaj <r.arunmr@gmail.com>
2016-07-11 22:36:33 +0530

    Refactored Code

--- WCF Service/ConsoleClient/Program.cs
+++ WCF Service/ConsoleClient/Program.cs
@@ -10,12 +10,15 @@ namespace ConsoleClient
     {
         static void Main()
         {
+            //Get user input to select the Type of Binding
             Console.WriteLine("Choose Binding:\n1.BasicHttpBinding\n2.WSHttpBinding\n3.NetHttpBinding\n4.NetTcpBinding");
             int choice = int.Parse(Console.ReadLine()) - 1;
 
+            //Calling the Simple Factory to create object of the required Binding
             AbstractSoapClientBindings clientBindings = SimpleBindingFactory.GetBindingFactory((BindingTypes)choice);
-            IEmployeeOperations proxy = ChannelFactory<IEmployeeOperations>.CreateChannel(clientBindings.BindingType, clientBindings.EndPoint);
 
+            //Calling the client by creating a proxy using the slected binding
+            IEmployeeOperations proxy = ChannelFactory<IEmployeeOperations>.CreateChannel(clientBindings.BindingType, clientBindings.EndPoint);
             Employee employee = new Employee { EmployeeName = "Arun", EmployeePay = "30k", EmployeeRole = "SoftwareEngineer" };
             bool isAdded = proxy.AddEmployee(employee);
             if (isAdded)
